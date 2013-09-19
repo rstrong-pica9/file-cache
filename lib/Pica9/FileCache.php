a09baede56439626c15decf8feeec883fe593190
Rob Strong <robert.a.strong@gmail.com>
2013-09-19 08:25:12 -0400

    initial commit

---
+++ lib/Pica9/FileCache.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Pica;
+
+class FileCache
+{
+    public function getFile($uniqueId)
+    {
+
+    }
+
+    public function storeFile($uniqueId, $fileData)
+    {
+
+    }
+
+    public function fileIsCached($uniqueId)
+    {
+
+    }
+}
